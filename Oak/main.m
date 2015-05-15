cebb03bc7c0dfdcd5ca918178d3febd2bd4149b5
Tomohiro Matsumura <t-matsumura@entaworks.co.jp>
2015-05-15 09:47:00 +0900

    Create project

---
+++ Oak/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Oak
+//
+//  Created by t-matsumura on 5/15/15.
+//  Copyright (c) 2015 Humour Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
