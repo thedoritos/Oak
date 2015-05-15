89e9baf34e7d3da51aae71b0baf151f9d286ee35
thedoritos <thedoritos@gmail.com>
2015-05-15 09:47:00 +0900

    Create project

---
+++ Oak/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Oak
+//
+//  Created by t-matsumura on 5/15/15.
+//  Copyright (c) 2015 Humour Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Oak/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Oak
+//
+//  Created by t-matsumura on 5/15/15.
+//  Copyright (c) 2015 Humour Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
