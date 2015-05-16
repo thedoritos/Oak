61f342c29649d15321495d55712492800a287fb7
Tomohiro Matsumura <t-matsumura@entaworks.co.jp>
2015-05-16 10:59:47 +0900

    Show in Navigation Controller

--- Oak/AppDelegate.m
+++ Oak/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -14,9 +15,16 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    UIViewController *defaultViewController = [[ViewController alloc] init];
+    UIViewController *rootViewController = [[UINavigationController alloc] initWithRootViewController:defaultViewController];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
