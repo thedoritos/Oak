2c7f66b350c2a9eb27b5a74f217e777f2793e5e6
thedoritos <thedoritos@gmail.com>
2015-05-16 15:54:14 +0900

    Only build on change on develop branch

--- Travis/Scripts/build_ipa.sh
+++ Travis/Scripts/build_ipa.sh
@@ -3,6 +3,9 @@
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
 exit 0
 fi
+if [[ "$TRAVIS_BRANCH" != "develop" ]]; then
+exit 0
+fi
 
 rm -rf ./build/*
 xcodebuild -workspace ${APP_NAME}.xcworkspace -scheme ${APP_NAME} -sdk iphoneos -configuration Release CODE_SIGN_IDENTITY="${DEVELOPER_NAME}" archive -archivePath ./build/${APP_NAME}.xcarchive
