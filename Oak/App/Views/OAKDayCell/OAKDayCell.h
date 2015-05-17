35f6acc822b7c3838fb9ed7505537813acdfbcb8
thedoritos <thedoritos@gmail.com>
2015-05-17 11:27:51 +0900

    Add day cell

---
+++ Oak/App/Views/OAKDayCell/OAKDayCell.h
@@ -0,0 +1,13 @@
+//
+//  OAKDayCell.h
+//  Oak
+//
+//  Created by t-matsumura on 5/17/15.
+//  Copyright (c) 2015 Humour Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OAKDayCell : UITableViewCell
+
+@end
---
+++ Oak/App/Views/OAKDayCell/OAKDayCell.m
@@ -0,0 +1,20 @@
+//
+//  OAKDayCell.m
+//  Oak
+//
+//  Created by t-matsumura on 5/17/15.
+//  Copyright (c) 2015 Humour Studio. All rights reserved.
+//
+
+#import "OAKDayCell.h"
+
+@interface OAKDayCell ()
+
+@property (weak, nonatomic) IBOutlet UILabel *dayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eventLabel;
+
+@end
+
+@implementation OAKDayCell
+
+@end
