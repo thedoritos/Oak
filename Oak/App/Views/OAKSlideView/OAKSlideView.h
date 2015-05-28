f006701bcab550f5ac54c3ccc5b6aece9ec6cbd0
thedoritos <thedoritos@gmail.com>
2015-05-29 00:36:51 +0900

    Add delegate to slide view

--- Oak/App/Views/OAKSlideView/OAKSlideView.h
+++ Oak/App/Views/OAKSlideView/OAKSlideView.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+@class OAKSlideView;
+
+@protocol OAKSlideViewDelegate <NSObject>
+
+- (void)slideViewDidSelectRight:(OAKSlideView *)slideView;
+- (void)slideViewDidSelectLeft:(OAKSlideView *)slideView;
+
+@end
+
 @interface OAKSlideView : UIView
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UIButton *leftButton;
+@property (weak, nonatomic) IBOutlet UIButton *rightButton;
+
+@property (nonatomic) id<OAKSlideViewDelegate> delegate;
 
 @end
--- Oak/App/Views/OAKSlideView/OAKSlideView.m
+++ Oak/App/Views/OAKSlideView/OAKSlideView.m
@@ -10,12 +10,37 @@
 
 @implementation OAKSlideView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+
 }
-*/
+
+-  (void)setup {
+    UIView *view = [[NSBundle mainBundle] loadNibNamed:@"OAKSlideView" owner:self options:nil][0];
+    
+    view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    view.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
+    
+    [self addSubview:view];
+    [self setNeedsUpdateConstraints];
+}
+
+#pragma mark - UIActions
+
+- (IBAction)didSelectLeft:(id)sender {
+    if (self.delegate) {
+        [self.delegate slideViewDidSelectLeft:self];
+    }
+}
+
+- (IBAction)didSelectRight:(id)sender {
+    if (self.delegate) {
+        [self.delegate slideViewDidSelectRight:self];
+    }
+}
 
 @end
