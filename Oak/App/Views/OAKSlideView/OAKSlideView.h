29480eb50be7e770f38797a20d3a8d0ddd15e347
thedoritos <thedoritos@gmail.com>
2015-05-28 23:55:22 +0900

    Add slider view

---
+++ Oak/App/Views/OAKSlideView/OAKSlideView.h
@@ -0,0 +1,13 @@
+//
+//  OAKSlideView.h
+//  Oak
+//
+//  Created by t-matsumura on 5/28/15.
+//  Copyright (c) 2015 Humour Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OAKSlideView : UIView
+
+@end
---
+++ Oak/App/Views/OAKSlideView/OAKSlideView.m
@@ -0,0 +1,21 @@
+//
+//  OAKSlideView.m
+//  Oak
+//
+//  Created by t-matsumura on 5/28/15.
+//  Copyright (c) 2015 Humour Studio. All rights reserved.
+//
+
+#import "OAKSlideView.h"
+
+@implementation OAKSlideView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
